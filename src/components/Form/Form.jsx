a5778b08bfc830abb89d11cba89a12a35de30d5c
Ignacio Waigandt <ignaawaig@gmail.com>
2022-12-10 10:25:46 -0300

    modified:   src/components/Form/Form.jsx 	new file:   src/components/Form/logo.png 	modified:   src/components/Nav.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,42 @@
 import { useState } from 'react'
 import validation from './validation';
+import styled from 'styled-components';
+import logo from './logo.png'
+
+const Container = styled.div`
+    width: 320px;
+    height: 420px;
+    background-color: white;
+    color: purple;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    box-sizing: border-box;
+    padding: 70px 30px;
+    border-radius: 8px;
+`
+
+const Img = styled.img`
+    width: 420px;
+    position: absolute;
+    top: -156px;
+    right: -50px;
+`
+
+const Title = styled.p`
+    margin: 0;
+    padding: 0 0 20px;
+    text-align: center;
+    font-size: 22px;
+`
+
+const Box = styled.div`
+    margin: 0;
+    padding: 0;
+    font-weight: bold;
+    display: block;
+`
 
 export default function Form(props){
 
@@ -24,14 +61,18 @@ export default function Form(props){
     }
 
     return (
-        <div>
-            <label>Username</label>
-            <input type="text" name='username' value={userData.username} onChange={handleInputChange}/>
-            <p>{errors.username}</p>
-            <label>Password</label>
-            <input type="password" name='password' value={userData.password} onChange={handleInputChange}/>
-            <p>{errors.password}</p>
-            <button onClick={handleSubmit}>LOGIN</button>
-        </div>
+        <Container>
+            <Img src={logo}/>
+            <Box>
+                <Title>¡Bienvenid@s a mi app!</Title>
+                <label>Username</label>
+                <input type="text" name='username' value={userData.username} onChange={handleInputChange}/>
+                <p>{errors.username}</p>
+                <label>Password</label>
+                <input type="password" name='password' value={userData.password} onChange={handleInputChange}/>
+                <p>{errors.password}</p>
+                <button onClick={handleSubmit}>LOGIN</button>
+            </Box>
+        </Container>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,90 @@
 import styled from "styled-components";
 import SearchBar from "./SearchBar.jsx";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
-const NavBar = styled.nav`
-  background-color: rgba(0, 0, 0, 50%);
-  opacity: 0.99;
+
+
+const NavbarContainer = styled.nav`
+  width: 100%;
+  height: ${(props) => (props.extendNavbar ? "100vh" : "80px")};
+  background-color: purple;
   display: flex;
-  padding-right: 15px;
+  flex-direction: column;
+  @media (min-width: 700px) {
+    height: 80px;
+  }
 `;
 
-const Bar = styled.div`
+const LeftContainer = styled.div`
+  flex: 70%;
+  display: flex;
+  align-items: center;
+  padding-left: 5%;
+`;
+
+const RightContainer = styled.div`
+  flex: 30%;
+  display: flex;
   justify-content: flex-end;
+  padding-right: 50px;
+`;
+
+const NavbarInnerContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+`;
+
+const NavbarLinkContainer = styled.div`
+  display: flex;
+`;
+
+const NavbarLink = styled(NavLink)`
+  color: white;
+  font-size: x-large;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+const NavbarLinkExtended = styled(NavLink)`
+  color: white;
+  font-size: x-large;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+`;
+
+const Logo = styled.img`
+  margin: 10px;
+  max-width: 180px;
+  height: auto;
+`;
+
+const OpenLinksButton = styled.button`
+  width: 70px;
+  height: 50px;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 45px;
+  cursor: pointer;
+  @media (min-width: 700px) {
+    display: none;
+  }
+`;
+
+const NavbarExtendedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (min-width: 700px) {
+    display: none;
+  }
 `;
 
 const Button = styled.button`
@@ -19,23 +93,47 @@ const Button = styled.button`
 `;
 
 export default function Nav(props) {
+
+  const [extendNavbar, setExtendNavbar] = useState(false);
+
   return (
     <div>
-      <NavBar>
-        <NavLink to={"/home"}>
-          <Button>Home</Button>
-        </NavLink>
-        <NavLink to={"/favorites"}>
-          <Button>Favorites</Button>
-        </NavLink>
-        <NavLink to={"/about"}>
-          <Button>About</Button>
-        </NavLink>
-        <Bar>
-          <SearchBar onSearch={props.onSearch} />
-        </Bar>
-        <Button onClick={props.logout}>Logout</Button>
-      </NavBar>
+      <NavbarContainer extendNavbar={extendNavbar}>
+        <NavbarInnerContainer>
+          <LeftContainer>
+            <NavbarLinkContainer>
+              <NavbarLink to={"/home"}>
+                <Button>Home</Button>
+              </NavbarLink>
+              <NavbarLink to={"/favorites"}>
+                <Button>Favorites</Button>
+              </NavbarLink>
+              <NavbarLink to={"/about"}>
+                <Button>About</Button>
+              </NavbarLink>
+                <SearchBar onSearch={props.onSearch} />
+              <OpenLinksButton
+              onClick={() => {
+                setExtendNavbar((curr) => !curr);
+              }}
+            >
+              {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+            </OpenLinksButton>
+            </NavbarLinkContainer>
+          </LeftContainer>
+          <RightContainer>
+            <Button onClick={props.logout}>Logout</Button>
+          </RightContainer>
+        </NavbarInnerContainer>
+        {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended to="/"> Home</NavbarLinkExtended>
+          <NavbarLinkExtended to="/products"> Products</NavbarLinkExtended>
+          <NavbarLinkExtended to="/contact"> Contact Us</NavbarLinkExtended>
+          <NavbarLinkExtended to="/about"> About Us</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
+      </NavbarContainer>
     </div>
   );
 }
